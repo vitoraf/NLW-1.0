141568c6ec9cb4330dde31d1dde3ea4174351bba
Vitor Alexandre Ferreira <vitor.ferreira@laureatedigital.net>
2020-08-10 17:36:19 -0300

    update

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children, ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import backIcon from '../../assets/images/icons/back.png';
@@ -8,10 +8,11 @@ import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps{
     title: string;
+    headerRight?: ReactNode;
 }
 
 
-const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
+const PageHeader: React.FC<PageHeaderProps> = ({title, headerRight, children}) => {
     const {navigate} = useNavigation();
     function handleGoBack(){
         navigate('Landing')
@@ -24,7 +25,12 @@ const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
             </BorderlessButton>
             <Image source={logoImg} resizeMode="contain" />
         </View>
-        <Text style={styles.title}>{title}</Text>
+        <View style={styles.header}>
+            <Text style={styles.title}>{title}</Text>
+            {headerRight}
+        </View>
+        
+        {children}
     </View>
     )
 }
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import {RectButton} from 'react-native-gesture-handler';
+import api from '../../Services/api';
 import styles from './styles'
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -10,6 +11,15 @@ import heartIcon from '../../assets/images/icons/heart.png'
 
 function Landing(){
     const {navigate} = useNavigation();
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response =>{
+            const { total } = response.data;
+            setTotalConnections(total);
+        })
+    }, []);
+    
     function handleNavigateToGiveClassesPage(){
         navigate('GiveClasses')
     }
@@ -41,7 +51,7 @@ function Landing(){
             </RectButton>
         </View>
         <Text style={styles.totalConnections}>
-            Total de 200 conexões já realizadas {' '}
+            Total de {totalConnections} conexões já realizadas {' '}
             <Image source={heartIcon}/>
         </Text>
     </View>
